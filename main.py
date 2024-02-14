cddbe595a5165d95373b4e5a9797d3c9600e3301
hackcode2022 <115550946+hackcode2022@users.noreply.github.com>
2024-02-14 01:41:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -93,6 +93,6 @@ import threading
 task_thread = threading.Thread(target=execute_planified_tasks)
 task_thread.start()
 if __name__ == '__main__':
-
-   app.run(host='0.0.0.0', port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Utilisation du port 8000 par défaut
+    app.run(host='0.0.0.0', port=port)
 
